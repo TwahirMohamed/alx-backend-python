83f924e0c877b2ac0e06788765880f0cfb25c397
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-25 20:13:29 -0500

    created user model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class User(models.Model):
+    class Role(models.TextChoices):
+        GUEST = 'guest', 'Guest'
+        HOST = 'host', 'Host'
+        ADMIN = 'admin', 'Admin'
+
+    user_Id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254, unique=True)
+    password_hash = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    role = models.CharField(max_length=10, choices=Role.choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.email}"
+
