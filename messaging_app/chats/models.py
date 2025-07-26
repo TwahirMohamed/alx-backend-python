1e9f1307dc45d6b1a48ee560711cf0a1b3135b8c
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-25 20:44:09 -0500

    task 1

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,40 +1,47 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 # Create your models here.
-class User(models.Model):
-    class Role(models.TextChoices):
-        GUEST = 'guest', 'Guest'
-        HOST = 'host', 'Host'
-        ADMIN = 'admin', 'Admin'
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('guest', 'Guest'),
+        ('host', 'Host'),
+        ('admin', 'Admin'),
+    )
 
     user_Id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    email = models.EmailField(max_length=254, unique=True)
+    # first_name = models.CharField(max_length=200)
+    # last_name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
     password_hash = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
-    role = models.CharField(max_length=10, choices=Role.choices)
-    created_at = models.DateTimeField(auto_now_add=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='guest' )
+    created_at = models.DateTimeField(default=timezone.now)
+
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+    USERNAME_FIELD = 'username'
+
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} {self.email}"
+        return f"{self.username} {self.role}"
 
 
 class Message(models.Model):
     message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
     sender_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
     message_body = models.TextField(max_length=500)
-    sent_at = models.DateTimeField(auto_now_add=True)
+    sent_at = models.DateTimeField(default=timezone.now)
 
 
     def __str__(self):
-        return f"{self.message_body} {self.sent_at}{self.sender_id}"
+        return f"Message:{self.message_body} Sent at{self.sent_at} from{self.sender_id}"
 
 class Conversation(models.Model):
     conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     participants_id = models.ForeignKey(User, on_delete=models.CASCADE,related_name='conversations' )
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f"{self.conversation_id}{self.participants_id}"
+        return f"Conversation {self.conversation_id}from{self.participants_id}"
