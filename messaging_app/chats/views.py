19e91f191686e8e2d8ceeec33bdd7140fcd56dcf
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-26 08:58:30 -0500

    added views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
+
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    @action(detail=True, methods=['post'])
+    def add_message(self, request, pk=None):
+        """
+        Send a new message to an existing conversation
+        Endpoint: /api/conversations/{conversation_id}/add_message/
+        """
+        conversation = self.get_object()
+        serializer = MessageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(conversation=conversation, sender=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        # Optional: filter messages by conversation using query params
+        conversation_id = self.request.query_params.get('conversation')
+        if conversation_id:
+            return self.queryset.filter(conversation__id=conversation_id)
+        return self.queryset
+
 
 # Create your views here.
