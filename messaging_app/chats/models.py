beef4576c346178f1e91a4b8751ecd1cd63181b0
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-25 20:31:37 -0500

    created message and conversation model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -20,3 +20,21 @@ class User(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name} {self.email}"
 
+
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
+    sender_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
+    message_body = models.TextField(max_length=500)
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.message_body} {self.sent_at}{self.sender_id}"
+
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants_id = models.ForeignKey(User, on_delete=models.CASCADE,related_name='conversations' )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.conversation_id}{self.participants_id}"
