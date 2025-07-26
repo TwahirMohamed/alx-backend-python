ee545db264b0186c2194fa86cb4920316fa885e6
MohamedTwahir <mohamedtwahir977@gmail.com>
2025-07-25 20:46:36 -0500

    fixed error

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -11,7 +11,7 @@ class User(AbstractUser):
         ('admin', 'Admin'),
     )
 
-    user_Id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, db_index=True)
     # first_name = models.CharField(max_length=200)
     # last_name = models.CharField(max_length=200)
     email = models.EmailField(unique=True)
